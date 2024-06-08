6de8928b94e8057ff901673049a0a378e59342a9
wawan <muhaguskurniawan@gmail.com>
2024-06-08 22:03:30 +0700

    selesai

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './LE SSERAFIM.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LE SSERAFIM
+        </p> <br></br>
+        <p>
+          Le Sserafim (/ləːˈsəʁafɪm/; Hangul: 르세라핌; RR: Reuserapim; MR: Rŭserap'im; bergaya dalam semua huruf besar) adalah grup vokal wanita pertama hasil kolaborasi Source Music dan Hybe Corporation. Grup ini beranggotakan 5 orang, yaitu Sakura, Chae-won, Yun-jin, Kazuha, dan Eun-chae. Le Sserafim debut dengan album mini pertama "Fearless" dengan judul lagu yang sama pada 2 Mei 2022. Sebelumnya Pada 28 Maret 2022 jam 00.00 (WKS) / 27 Mar 2022 22.00 (WIB), Le Sserafim merilis logo resmi sebelum melakukan debut.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
